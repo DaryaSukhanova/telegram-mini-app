6c79e631844c470aab8e477a54a160853aa3cab9
Sukhanova Darya <daria_15tit@mail.ru>
2024-07-02 14:56:28 +0300

    added user data

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,41 +1,38 @@
 import { FC, HTMLAttributes } from 'react';
 import './Header.css';
-import { useInitData, User } from '@tma.js/sdk-react';
-// interface User {
-//     name: string;
-//     avatar: string;
-//     level: number;
-// }
+interface User {
+    name: string;
+    avatar: string;
+    level: number;
+}
 
 interface HeaderProps extends HTMLAttributes<HTMLElement> {
     points: number;
     maxScore: number;
 }
 
-// const user: User = {
-//     name: 'Anna',
-//     avatar: 'avatar.png',
-//     level: 15,
-// };
+const user: User = {
+    name: 'Anna',
+    avatar: 'avatar.png',
+    level: 15,
+};
 
 export const Header: FC<HeaderProps> = ({ points, maxScore }) => {
-  const initData = useInitData();
-  const user: User | undefined = initData?.user;
     const progress = Math.min((points / maxScore) * 100, 100);
 
   return (
     <div className="header-container">
       <div className="user-info">
-        <img className="avatar" src={user?.photoUrl} alt="Profile" />
-        <span className="user-name">{user?.firstName} {user?.lastName}</span>
+        <img className="avatar" src={user.avatar} alt="Profile" />
+        <span className="user-name">{user.name}</span>
       </div>
       <div className='level-info'>
         <div className="level-details">
-            <span className="level">15 level</span>
+            <span className="level">{user.level} level</span>
             <div className="score-info">
                 <img className="coin" src="coin.png" alt="CoinImg" />
                 <span className="score">{maxScore.toLocaleString()}</span>
-                <span className="to-next-level">To 16 lvl</span>
+                <span className="to-next-level">To {user.level+1} lvl</span>
             </div>
         </div>
 
